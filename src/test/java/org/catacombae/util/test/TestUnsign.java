81ca03268d01ea90d2d15a1d558c15df046ac6f0
Naohide Sano <namachapanda2005@gmail.com>
2022-03-09 22:00:47 +0900

    add junit test

--- src/test/java/org/catacombae/util/test/TestUnsign.java
+++ src/test/java/org/catacombae/util/test/TestUnsign.java
@@ -5,58 +5,66 @@
 
 package org.catacombae.util.test;
 
+import java.math.BigInteger;
+
 import org.catacombae.util.Util;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 /**
  *
  * @author erik
  */
-public class TestUnsign {
-    public static void main(String[] args) {
+class TestUnsign {
+
+    @Test
+    void test() {
         byte bLow = 0x0, bMiddle = 0x7F, bHigh = (byte)0xFF;
         short sLow = 0x0, sMiddle = 0x7FFF, sHigh = (short)0xFFFF;
         char cLow = 0x0, cMiddle = 0x7FFF, cHigh = 0xFFFF;
         int iLow = 0x0, iMiddle = 0x7FFFFFFF, iHigh = 0xFFFFFFFF;
         long lLow = 0x0, lMiddle = 0x7FFFFFFFFFFFFFFFL, lHigh = 0xFFFFFFFFFFFFFFFFL;
 
-        System.out.println("byte:");
-        System.out.println("  Low (natural): " + bLow);
-        System.out.println("  Low (unsigned): " + Util.unsign(bLow));
-        System.out.println("  Middle (natural): " + bMiddle);
-        System.out.println("  Middle (unsigned): " + Util.unsign(bMiddle));
-        System.out.println("  High (natural): " + bHigh);
-        System.out.println("  High (unsigned): " + Util.unsign(bHigh));
+        // byte
+        assertEquals(0, bLow);
+        assertEquals(0, Util.unsign(bLow));
+        assertEquals(127, bMiddle);
+        assertEquals(127, Util.unsign(bMiddle));
+        assertEquals(-1, bHigh);
+        assertEquals(255, Util.unsign(bHigh));
 
-        System.out.println("short:");
-        System.out.println("  Low (natural): " + sLow);
-        System.out.println("  Low (unsigned): " + Util.unsign(sLow));
-        System.out.println("  Middle (natural): " + sMiddle);
-        System.out.println("  Middle (unsigned): " + Util.unsign(sMiddle));
-        System.out.println("  High (natural): " + sHigh);
-        System.out.println("  High (unsigned): " + Util.unsign(sHigh));
+        // short
+        assertEquals(0, sLow);
+        assertEquals(0, Util.unsign(sLow));
+        assertEquals(32767, sMiddle);
+        assertEquals(32767, Util.unsign(sMiddle));
+        assertEquals(-1, sHigh);
+        assertEquals(65535, Util.unsign(sHigh));
 
-        System.out.println("char:");
-        System.out.println("  Low (natural): " + cLow);
-        System.out.println("  Low (unsigned): " + Util.unsign(cLow));
-        System.out.println("  Middle (natural): " + cMiddle);
-        System.out.println("  Middle (unsigned): " + Util.unsign(cMiddle));
-        System.out.println("  High (natural): " + cHigh);
-        System.out.println("  High (unsigned): " + Util.unsign(cHigh));
+        // char
+        assertEquals((char) 0, cLow);
+        assertEquals(0, Util.unsign(cLow));
+        assertEquals('翿', cMiddle);
+        assertEquals(32767, Util.unsign(cMiddle));
+        assertEquals('￿', cHigh);
+        assertEquals(65535, Util.unsign(cHigh));
 
-        System.out.println("int:");
-        System.out.println("  Low (natural): " + iLow);
-        System.out.println("  Low (unsigned): " + Util.unsign(iLow));
-        System.out.println("  Middle (natural): " + iMiddle);
-        System.out.println("  Middle (unsigned): " + Util.unsign(iMiddle));
-        System.out.println("  High (natural): " + iHigh);
-        System.out.println("  High (unsigned): " + Util.unsign(iHigh));
+        // int
+        assertEquals(0, iLow);
+        assertEquals(0, Util.unsign(iLow));
+        assertEquals(2147483647, iMiddle);
+        assertEquals(2147483647, Util.unsign(iMiddle));
+        assertEquals(-1, iHigh);
+        assertEquals(4294967295l, Util.unsign(iHigh));
 
-        System.out.println("long:");
-        System.out.println("  Low (natural): " + lLow);
-        System.out.println("  Low (unsigned): " + Util.unsign(lLow));
-        System.out.println("  Middle (natural): " + lMiddle);
-        System.out.println("  Middle (unsigned): " + Util.unsign(lMiddle));
-        System.out.println("  High (natural): " + lHigh);
-        System.out.println("  High (unsigned): " + Util.unsign(lHigh));
+        // long
+        assertEquals(0, lLow);
+        assertEquals(BigInteger.ZERO, Util.unsign(lLow));
+        assertEquals(9223372036854775807l, lMiddle);
+        assertEquals(BigInteger.valueOf(9223372036854775807l), Util.unsign(lMiddle));
+        assertEquals(-1, lHigh);
+        assertEquals(new BigInteger("18446744073709551615"), Util.unsign(lHigh));
     }
 }
